24d112504b9ce6812ce18a194db160572da830fb
Arely Viana <areviana17@gmail.com>
2020-09-16 10:01:49 -0600

    🚀 Prepare for production

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   const options = new DocumentBuilder()
     .setTitle('OAuth2 example')
     .setDescription('The OAuth 2.0 API example description')
@@ -13,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
